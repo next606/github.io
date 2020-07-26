a59615be988397496778753e6c31c6da32ae004c
next606 <56588992+next606@users.noreply.github.com>
2020-07-26 14:10:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+/* header active */
+let elements = document.getElementsByClassName("header__item");
 
+    for (var i = 0; i < elements.length; i++){
+        elements[i].onclick = function () {
+            var el = elements[0];
+            while(el){                
+                el.classList.remove("header__link-active");             
+                el = el.nextElementSibling;
+            }
+            this.classList.add("header__link-active");
+        }
+    }
 
   /* accordion */
   
@@ -10,7 +22,6 @@
         .slideToggle(200);
     });
   });
-
 
 
 
@@ -29,42 +40,184 @@
 
  /* burger  */
 
-(function(){
+/* (function(){
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav-box')
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-box-active');
     });
     menuCloseItem.addEventListener('click', () => {
       menu.classList.remove('header__nav-box-active');
     });
-}());
+    if (window.innerWidth <= 1000) {
+      for (let i = 0; i < menuLinks.length; i += 1) {
+          menuLinks[i].addEventListener('click', () => {
+              menu.classList.remove('header__nav-box-active');
+          });
+      }
+  }
+}()); */
 
 
 /* gallery */
 
-  $('.gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      }
+
+/* $('.project__gallery').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  tLoading: 'Loading image #%curr%...',
+  mainClass: 'mfp-img-mobile',
+  gallery: {
+    enabled: true,
+    navigateByImgClick: true,
+    preload: [0,1] 
+  },
+  image: {
+    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    titleSrc: function(item) {
+      return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    }
+  }
+}); */
+
+
+// Scroll to anchors main
+(function () {
+
+  const smoothScroll = function (targetEl, duration) {
+      const headerElHeight =  document.querySelector('.header').clientHeight;
+      let target = document.querySelector(targetEl);
+      let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+      let startPosition = window.pageYOffset;
+      let startTime = null;
+  
+      const ease = function(t,b,c,d) {
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+  
+      const animation = function(currentTime){
+          if (startTime === null) startTime = currentTime;
+          const timeElapsed = currentTime - startTime;
+          const run = ease(timeElapsed, startPosition, targetPosition, duration);
+          window.scrollTo(0,run);
+          if (timeElapsed < duration) requestAnimationFrame(animation);
+      };
+      requestAnimationFrame(animation);
+
+  };
+
+  const scrollTo = function () {
+      const links = document.querySelectorAll('.js-scroll');
+      links.forEach(each => {
+          each.addEventListener('click', function () {
+              const currentTarget = this.getAttribute('href');
+              smoothScroll(currentTarget, 1000);
+          });
+      });
+  };
+  scrollTo();
+}());
+
+/* Scroll to anchors small */
+
+(function () {
+
+  const smoothScroll = function (targetEl, duration) {
+      const headerElHeight =  document.querySelector('.about__btn').clientHeight;
+      let target = document.querySelector(targetEl);
+      let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+      let startPosition = window.pageYOffset;
+      let startTime = null;
+  
+      const ease = function(t,b,c,d) {
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+  
+      const animation = function(currentTime){
+          if (startTime === null) startTime = currentTime;
+          const timeElapsed = currentTime - startTime;
+          const run = ease(timeElapsed, startPosition, targetPosition, duration);
+          window.scrollTo(0,run);
+          if (timeElapsed < duration) requestAnimationFrame(animation);
+      };
+      requestAnimationFrame(animation);
+
+  };
+
+  const scrollTo = function () {
+      const links = document.querySelectorAll('.js-scroll2');
+      links.forEach(each => {
+          each.addEventListener('click', function () {
+              const currentTarget = this.getAttribute('href');
+              smoothScroll(currentTarget, 1000);
+          });
+      });
+  };
+  scrollTo();
+}());
+
+/* video */
+
+$('.card-deck a').fancybox({
+    caption : function( instance, item ) {
+      return $(this).parent().find('.card-text').html();
     }
   });
 
+/* photoswipe */
+(function($) {
+  var $pswp = $('.pswp')[0];
+  var image = [];
 
+  $('.picture').each( function() {
+      var $pic     = $(this),
+          getItems = function() {
+              var items = [];
+              $pic.find('a').each(function() {
+                  var $href   = $(this).attr('href'),
+                      $size   = $(this).data('size').split('x'),
+                      $width  = $size[0],
+                      $height = $size[1];
 
-});
+                  var item = {
+                      src : $href,
+                      w   : $width,
+                      h   : $height
+                  }
 
+                  items.push(item);
+              });
+              return items;
+          }
 
+      var items = getItems();
+
+      $.each(items, function(index, value) {
+          image[index]     = new Image();
+          image[index].src = value['src'];
+      });
+
+      $pic.on('click', 'figure', function(event) {
+          event.preventDefault();
+          
+          var $index = $(this).index();
+          var options = {
+              index: $index,
+              bgOpacity: 0.7,
+              showHideOpacity: true
+          }
+
+          var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+          lightBox.init();
+      });
+  });
+})(jQuery);
 
