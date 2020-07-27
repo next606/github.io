ef05e581035b67b4f082e9fbd2034be169d2480d
next606 <56588992+next606@users.noreply.github.com>
2020-07-27 20:16:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -211,6 +211,28 @@ $(document).ready(function() {
 });
 
 
+/* send form */
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
 
 
 
