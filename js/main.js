86100021eeeacced63e4f469578867ef7a4b563c
next606 <56588992+next606@users.noreply.github.com>
2020-07-29 16:32:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -215,6 +215,80 @@ $(document).ready(function() {
 });
 
 
+/* contact form mask */
+$('.phone').mask('+7 (000) 000-0000', {placeholder: "Введите номер телефона"});
+
+
+$('.crazy_cep').mask('00000-000', { onKeyPress: function (cep) {
+  var masks = ['00000-000', '0-00-00-00'];
+  mask = (cep.length>7) ? masks[1] : masks[0];
+  $('.crazy_cep').mask(mask, this);
+}});
+
+$('.cpf').mask('000.000.000-00', {reverse: true});
+
+var SaoPauloCelphoneMask = function(phone, e, currentField, options){
+  return phone.match(/^(\(?11\)? ?9(5[0-9]|6[0-9]|7[01234569]|8[0-9]|9[0-9])[0-9]{1})/g) ?
+    '(00) 00000-0000' : '(00) 0000-0000';
+};
+
+$(".sp_celphones").mask('(00) 00009-0000', {placeholder: "( __ ) _____ - ____"});
+
+$(".bt-mask-it").click(function(){
+  $(".mask-on-div").mask("000.000.000-00");
+  $(".mask-on-div").fadeOut(500).fadeIn(500)
+});
+
+/* footer policy */
+
+/* $(document).ready(function() {
+	$('.footer__text').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+});
+
+
+
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}  */
+
+
 
 
 
